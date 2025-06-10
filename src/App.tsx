d6e2587cfbbd5e7283e174c38b022dad4ea0720d
Gabriel Matos <gabriel.lz.mts@gmail.com>
2025-06-10 07:41:32 -0300

    added children props

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import { Heading } from './components/Heading';
 export function App() {
 	return (
 		<>
-			<Heading />
+			<Heading>Hello World</Heading>
 			<p>
 				Lorem ipsum dolor sit amet consectetur adipisicing elit. Id vel aliquid vero molestiae praesentium ipsum ea sed
 				voluptate nam cum dolorem eligendi optio veniam consequuntur doloribus, reprehenderit error est fugiat.
