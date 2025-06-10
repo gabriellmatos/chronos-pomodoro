faa91802793b071f11c14cbdf6331b3b5fb39bc3
Gabriel Matos <gabriel.lz.mts@gmail.com>
2025-06-10 07:22:10 -0300

    importing and using CSS modules with scope

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,16 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
-import './App.css';
+import './styles/theme.css';
+import './styles/global.css';
 
-function App() {
-	const [count, setCount] = useState(0);
+import { Heading } from './components/Heading';
 
+export function App() {
 	return (
 		<>
-			<div>
-				<a href='https://vite.dev' target='_blank'>
-					<img src={viteLogo} className='logo' alt='Vite logo' />
-				</a>
-				<a href='https://react.dev' target='_blank'>
-					<img src={reactLogo} className='logo react' alt='React logo' />
-				</a>
-			</div>
-			<h1>Vite + React</h1>
-			<div className='card'>
-				<button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
-				<p>
-					Edit <code>src/App.tsx</code> and save to test HMR
-				</p>
-			</div>
+			<Heading />
+			<p>
+				Lorem ipsum dolor sit amet consectetur adipisicing elit. Id vel aliquid vero molestiae praesentium ipsum ea sed
+				voluptate nam cum dolorem eligendi optio veniam consequuntur doloribus, reprehenderit error est fugiat.
+			</p>
 		</>
 	);
 }
-
-export default App;
