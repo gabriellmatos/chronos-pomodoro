8913679f2ff6a84b6539bb1821a977bf6ef45e0f
Gabriel Matos <gabriel.lz.mts@gmail.com>
2025-06-10 09:42:13 -0300

    defined component type using ReactNode

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,17 @@ import './styles/theme.css';
 import './styles/global.css';
 
 import { Heading } from './components/Heading';
+import { TimerIcon } from 'lucide-react';
 
 export function App() {
 	return (
 		<>
-			<Heading>Hello World</Heading>
+			<Heading>
+				Hello World
+				<button>
+					<TimerIcon />
+				</button>
+			</Heading>
 			<p>
 				Lorem ipsum dolor sit amet consectetur adipisicing elit. Id vel aliquid vero molestiae praesentium ipsum ea sed
 				voluptate nam cum dolorem eligendi optio veniam consequuntur doloribus, reprehenderit error est fugiat.
