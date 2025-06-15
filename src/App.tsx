ba0b93355a892a30c8fc2f60bbfe418593122119
Gabriel Matos <gabriel.lz.mts@gmail.com>
2025-06-15 17:13:34 -0300

    creating HTML and CSS structure for each section

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,30 @@
 import './styles/theme.css';
 import './styles/global.css';
 
-import { Heading } from './components/Heading';
-import { TimerIcon } from 'lucide-react';
-
 export function App() {
 	return (
 		<>
-			<Heading>
-				Hello World
-				<button>
-					<TimerIcon />
-				</button>
-			</Heading>
-			<p>
-				Lorem ipsum dolor sit amet consectetur adipisicing elit. Id vel aliquid vero molestiae praesentium ipsum ea sed
-				voluptate nam cum dolorem eligendi optio veniam consequuntur doloribus, reprehenderit error est fugiat.
-			</p>
+			<div className='container'>
+				<div className='content'>
+					<section>LOGO</section>
+				</div>
+			</div>
+			<div className='container'>
+				<div className='content'>
+					<section>MENU</section>
+				</div>
+			</div>
+
+			<div className='container'>
+				<div className='content'>
+					<section>FORM</section>
+				</div>
+			</div>
+			<div className='container'>
+				<div className='content'>
+					<section>FOOTER</section>
+				</div>
+			</div>
 		</>
 	);
 }
