d3176fee3cd77419ae6e6fefe27623b1945e4062
Gabriel Matos <gabriel.lz.mts@gmail.com>
2025-06-15 17:25:20 -0300

    moving container to a react component

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,22 @@
 import './styles/theme.css';
 import './styles/global.css';
+import { Container } from './components/Container';
 
 export function App() {
 	return (
 		<>
-			<div className='container'>
-				<div className='content'>
-					<section>LOGO</section>
-				</div>
-			</div>
-			<div className='container'>
-				<div className='content'>
-					<section>MENU</section>
-				</div>
-			</div>
-
-			<div className='container'>
-				<div className='content'>
-					<section>FORM</section>
-				</div>
-			</div>
-			<div className='container'>
-				<div className='content'>
-					<section>FOOTER</section>
-				</div>
-			</div>
+			<Container>
+				<section>LOGO</section>
+			</Container>
+			<Container>
+				<section>MENU</section>
+			</Container>
+			<Container>
+				<section>FORM</section>
+			</Container>
+			<Container>
+				<section>FOOTER</section>
+			</Container>
 		</>
 	);
 }
